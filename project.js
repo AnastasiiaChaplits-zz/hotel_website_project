9fba0bb1218941178600133088937e0b391d3602
Lin Ansty <anastasia.chaplits@gmail.com>
2020-01-05 00:49:59 +0200

    added home page as images gallery with javascript

---
+++ project.js
@@ -0,0 +1,21 @@
+function imagesGalleryHomePage() {
+    let images = [];
+    let i = 0;
+
+    images[0] = "Images/main page/main_1.jpg";
+    images[1] = "Images/main page/main_2.jpg";
+    images[2] = "Images/main page/main_3.jpg";
+    images[3] = "Images/main page/main_4.jpg";
+
+    return function() {
+        document.querySelector('.img_main').src = images[i];
+        i++;
+
+        if (i == images.length) {
+            i = 0;
+        }
+        setTimeout("viewImages()", 2000);
+    }
+}
+let viewImages = imagesGalleryHomePage();
+viewImages();
