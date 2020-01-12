8eeb6ef0eb0b06c45b86fe6a7df88497c557d5c9
Lin Ansty <anastasia.chaplits@gmail.com>
2020-01-12 15:02:06 +0200

    added new HTML page - Dinner Parties

--- hoteldining_content.js
+++ hoteldining_content.js
@@ -1,19 +1,20 @@
 //for Dining page - show working hours
-let showWorkingHours = document.querySelector('.hotel--dining_schedule');
+(function() {
+    let showWorkingHours = document.querySelector('.hotel--dining_schedule');
 
-showWorkingHours.addEventListener('click', function (event) {
-    let scheduleHoursElement = event.target.closest('.hotel--dining_schedule_hours');
-    if (!scheduleHoursElement) {
-        return;
-    }
-    if (scheduleHoursElement.nextElementSibling.style.display === 'flex') {
-        scheduleHoursElement.nextElementSibling.style.display = 'none';
-    } else {
-        scheduleHoursElement.nextElementSibling.style.display = 'flex';
+    showWorkingHours.addEventListener('click', function (event) {
+        let scheduleHoursElement = event.target.closest('.hotel--dining_schedule_hours');
+        if (!scheduleHoursElement) {
+            return;
+        }
+        if (scheduleHoursElement.nextElementSibling.style.display === 'flex') {
+            scheduleHoursElement.nextElementSibling.style.display = 'none';
+        } else {
+            scheduleHoursElement.nextElementSibling.style.display = 'flex';
 
-    }
+        }
 
 
-});
-
+    });
+}());
 
